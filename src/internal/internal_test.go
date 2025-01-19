b3a1fcaa88639b9b79303d2241a419646381285a
martabal <74269598+martabal@users.noreply.github.com>
2025-01-19 16:24:46 +0100

    test: add tests for `IsValidHttpsURL`

--- src/internal/internal_test.go
+++ src/internal/internal_test.go
@@ -34,6 +34,38 @@ func TestIsValidURL(t *testing.T) {
 	}
 }
 
+func TestIsValidHttpsURL(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected bool
+	}{
+		{"https://www.example.com", true},
+		{"http://localhost:8080", false},
+		{"ftp://ftp.example.com", false},
+		{"not_a_url", false},
+		{"www.example.com", false},
+		{"file:///path/to/file", false},
+		{"", false},
+		{"http://invalid..url", false},
+		{"https://[::1]", true},
+		{"http://user:pass@www.example.com", false},
+		{"https://www.example.com:8080/path", true},
+		{"https://www.example.com?query=value", true},
+		{"https://www.example.com#fragment", true},
+		{"http://www.ex ample.com", false},
+		{"http://www.example.com:10000", false},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.input, func(t *testing.T) {
+			result := IsValidHttpsURL(tc.input)
+			if result != tc.expected {
+				t.Errorf("Expected %s to be %v, but got %v", tc.input, tc.expected, result)
+			}
+		})
+	}
+}
+
 func TestEnsureLeadingSlash(t *testing.T) {
 	tests := []struct {
 		name           string
